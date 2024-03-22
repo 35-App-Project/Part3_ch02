b4890a7c59d902d22b5be5a34ce6ccfbdc7752ae
harry <harry7408@naver.com>
2024-03-22 11:40:10 +0900

    feat: clip2 ~ clip4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,7 +36,7 @@ android {
     }
 
     buildFeatures {
-        viewBinding=true
+        dataBinding=true
     }
 }
 
@@ -49,7 +50,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
 
 }
